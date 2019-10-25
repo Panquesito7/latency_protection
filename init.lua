700751c03ff9cf8c0f1e8324eadda308d26cae72
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-10-25 12:25:05 -0500

    Check: for player

--- init.lua
+++ init.lua
@@ -4,14 +4,14 @@ local timer = tonumber(minetest.settings:get("latency_protection.timer")) or 20
 local jitter_max = tonumber(minetest.settings:get("latency_protection.jitter_max")) or 1.5
 
 local function step()
-	for _, player in pairs(minetest.get_connected_players()) do
-		local name = player:get_player_name()
+	for name, data in players_position do
 		local info = minetest.get_player_information(name)
-		if not players_position[name].protection_violation and info.avg_jitter <= jitter_max then
-			players_position[name].pos = player:get_pos()
+		if not data[name].protection_violation and info.avg_jitter <= jitter_max then
+			data[name].pos = player:get_pos()
 		else
-			players_position[name].protection_violation = false
+			data[name].protection_violation = false
 		end
+		players_position[name] = data
 	end
 	minetest.after(timer, step)
 end
@@ -26,14 +26,18 @@ end)
 
 minetest.after(timer, step)
 
--- It has to register later for it to work.
+-- It has to be registered later for it to work.
 minetest.register_on_mods_loaded(function()
 	local old_is_protected = minetest.is_protected
 
 	function minetest.is_protected(pos, name)
 		local results = old_is_protected(pos, name)
 		if results then
-			minetest.get_player_by_name(name):set_pos(players_position[name].pos)
+			local player = minetest.get_player_by_name(name)
+			if not player then
+				return results
+			end
+			player:set_pos(players_position[name].pos)
 			players_position[name].protection_violation = true
 		end
 		return results
