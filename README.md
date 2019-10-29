ac58a3501d05048b3c4ea8db41b0eb1b4304daf2
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-10-28 23:46:20 -0500

    Add:  damage mode

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
 # Latency Protection
 
-This mod is an attempt to prevent players from glitching through protected nodes. By recording position every 20 seconds (This can be changed in settings) and teleporting them if the protection interaction happens in a every quick time frame. If a player interacts with a protected node the position will not be recorded for an extra cycle. Before recording a position, the player’s avg_jitter is checked to make sure the player is not lagging out.
+# Damage mode
+This mode attempts to prevent players from glitching through protected nodes. By damaging players who interact with a protected position too fast.
+
+# Teleport mode
+This mode attempts to prevent players from glitching through protected nodes. By recording position every 20 seconds (This can be changed in settings) and teleporting them if the protection interaction happens in a very quick time frame. If a player interacts with a protected node the position will not be recorded for an extra cycle. Before recording a position, the player’s avg_jitter is checked to make sure the player is not lagging out.
 
 The timer for when to record a player position.
+This only works if punishment is set to teleport.
 ``` lua
 latency_protection.timer = 20
 ```
 
 The max jitter a player can have before refusing the position update.
+This only works if punishment is set to teleport.
 ``` lua
 latency_protection.jitter_max = 1.5
 ```
 
-The time limit between is_protected calls. If the function is called too fast the player will be teleport.
+The time limit between is_protected calls. If the function is called too fast the player will be teleport or damaged.
 time_max is read in microseconds.
 ``` lua
 latency_protection.time_max = 2000
+```
+
+Damage amount to apply to the player.
+This only works if punishment is set to damage.
+``` lua
+latency_protection.damage = 3
+```
+
+Set what type of punishment to be given to a player.
+Teleport the player.
+Damage the player.
+``` lua
+latency_protection.punishment = damage
 ```
--- init.lua
+++ init.lua
@@ -1,55 +1,95 @@
 local players_position = {}
 
-local timer = tonumber(minetest.settings:get("latency_protection.timer")) or 20
-local jitter_max = tonumber(minetest.settings:get("latency_protection.jitter_max")) or 1.5
 local time_max = tonumber(minetest.settings:get("latency_protection.time_max")) or 2000
 
-local function step()
-	for name, data in pairs(players_position) do
-		local info = minetest.get_player_information(name)
-		if not data.protection_violation and info.avg_jitter <= jitter_max then
-			data.pos = minetest.get_player_by_name(name):get_pos()
-		else
-			data.protection_violation = false
-		end
-		players_position[name] = data
-	end
-	minetest.after(timer, step)
-end
+local function punishment_teleport()
+	local timer = tonumber(minetest.settings:get("latency_protection.timer")) or 20
+	local jitter_max = tonumber(minetest.settings:get("latency_protection.jitter_max")) or 1.5
 
-minetest.register_on_joinplayer(function(player)
-	players_position[player:get_player_name()] = {pos = player:get_pos(), protection_violation = false, last_time = 0}
-end)
-
-minetest.register_on_leaveplayer(function(player)
-	players_position[player:get_player_name()] = nil
-end)
-
-minetest.after(timer, step)
-
--- It has to be registered later for it to work.
-minetest.register_on_mods_loaded(function()
-	local old_is_protected = minetest.is_protected
-
-	function minetest.is_protected(pos, name)
-		local results = old_is_protected(pos, name)
-		local player = minetest.get_player_by_name(name)
-		if results and player then
-			local data = players_position[name]
-			local now = minetest.get_us_time()
-			-- If is_protected is called too quickly from the previous call, the player will be teleported.
-			if now - data.last_time <= time_max then
-				player:set_pos(data.pos)
-				data.protection_violation = true
+	local function step()
+		for name, data in pairs(players_position) do
+			local info = minetest.get_player_information(name)
+			if not data.protection_violation and info.avg_jitter <= jitter_max then
+				data.pos = minetest.get_player_by_name(name):get_pos()
+			else
+				data.protection_violation = false
 			end
-			data.last_time = now
 			players_position[name] = data
 		end
-		return results
+		minetest.after(timer, step)
 	end
 
-	minetest.register_on_respawnplayer(function(player)
-		players_position[player:get_player_name()].pos = player:get_pos()
-		return false
+	minetest.register_on_joinplayer(function(player)
+		players_position[player:get_player_name()] = {pos = player:get_pos(), protection_violation = false, last_time = 0}
 	end)
-end)
+
+	minetest.register_on_leaveplayer(function(player)
+		players_position[player:get_player_name()] = nil
+	end)
+
+	minetest.after(timer, step)
+
+	-- It has to be registered later for it to work.
+	minetest.register_on_mods_loaded(function()
+		local old_is_protected = minetest.is_protected
+
+		function minetest.is_protected(pos, name)
+			local results = old_is_protected(pos, name)
+			local player = minetest.get_player_by_name(name)
+			if results and player then
+				local data = players_position[name]
+				local now = minetest.get_us_time()
+				-- If is_protected is called too quickly from the previous call, the player will be teleported.
+				if now - data.last_time <= time_max then
+					player:set_pos(data.pos)
+					data.protection_violation = true
+				end
+				data.last_time = now
+				players_position[name] = data
+			end
+			return results
+		end
+
+		minetest.register_on_respawnplayer(function(player)
+			players_position[player:get_player_name()].pos = player:get_pos()
+			return false
+		end)
+	end)
+end
+
+local function punishment_damage()
+	local damage = tonumber(minetest.settings:get("latency_protection.damage")) or 3
+	
+	minetest.register_on_joinplayer(function(player)
+		players_position[player:get_player_name()] = minetest.get_us_time()
+	end)
+
+	minetest.register_on_leaveplayer(function(player)
+		players_position[player:get_player_name()] = nil
+	end)
+
+	-- It has to be registered later for it to work.
+	minetest.register_on_mods_loaded(function()
+		local old_is_protected = minetest.is_protected
+
+		function minetest.is_protected(pos, name)
+			local results = old_is_protected(pos, name)
+			local player = minetest.get_player_by_name(name)
+			if results and player then
+				local now = minetest.get_us_time()
+				-- If is_protected is called too quickly from the previous call, the player will be damaged.
+				if now - players_position[name] <= time_max then
+					player:set_hp(player:get_hp() - damage)
+				end
+				players_position[name] = now
+			end
+			return results
+		end
+	end)
+end
+
+if minetest.settings:get("latency_protection.punishment") == "teleport" then
+	punishment_teleport()
+else
+	punishment_damage()
+end
